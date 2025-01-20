8462beb1604b127185afef24a16cbb41251d1872
Yoann Robert <luneroka@gmail.com>
2025-01-20 20:51:14 +0100

    Rendering ContactForm

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -30,6 +30,15 @@ export const ContactsPage = ({ contacts, onAdd }) => {
     <div>
       <section>
         <h2>Add Contact</h2>
+        <ContactForm 
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
