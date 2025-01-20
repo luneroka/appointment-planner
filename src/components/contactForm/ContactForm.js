7fb6f037bcc1fbbff744228e3b19c704f58e928f
Yoann Robert <luneroka@gmail.com>
2025-01-20 21:07:23 +0100

    Implement ContactForm logic, values and onChange attributes

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from 'react';
 
 export const ContactForm = ({
   name,
@@ -7,10 +7,32 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={name}
+          placeholder='Name...'
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type='tel'
+          value={phone}
+          placeholder='Phone...'
+          pattern='^([\+][0-9]{1,3}([ \.\-])?)?([\(]{1}[0-9]{3}[\)])?([0-9A-Z \.\-]{1,32})((x|ext|extension)?[0-9]{1,4}?)$'
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <input
+          type='email'
+          value={email}
+          placeholder='Email...'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+    </>
   );
 };
-
