c0a8d3a8676e3e64a3166e9e9c3765f3f60b9a3e
Yoann Robert <luneroka@gmail.com>
2025-01-20 21:22:44 +0100

    Render TileList passing tiles prop to Tile component

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React from 'react';
+import { Tile } from '../tile/Tile';
 
-export const TileList = () => {
+export const TileList = ({ tiles }) => {
   return (
     <div>
-      
+      {tiles.map(({ name, ...rest }, index) => {
+        <Tile key={index} name={name} description={rest} />;
+      })}
     </div>
   );
 };
