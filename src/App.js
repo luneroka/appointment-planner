2017b03c94c4794d21594abdcfdc7209880e8aa3
Yoann Robert <luneroka@gmail.com>
2025-01-20 20:13:45 +0100

    Initialize state for contacts and appointments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  // Define state variables for contacts and appointments 
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
   /*
   Implement functions to add data to
