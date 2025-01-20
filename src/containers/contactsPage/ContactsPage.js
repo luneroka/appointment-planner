9226ed49557a8108bd1f33faa69e3aac0f1b7909
Yoann Robert <luneroka@gmail.com>
2025-01-20 20:32:30 +0100

    Initialize state for contact name, phone and email in ContactsPage component

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+export const ContactsPage = (props) => {
+  // Define state variables for contact info and duplicate check
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+ 
 
   const handleSubmit = (e) => {
     e.preventDefault();
